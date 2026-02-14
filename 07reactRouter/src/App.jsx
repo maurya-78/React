9ccd1fc5b2122ac94e770c984c245a624f7801db
maurya-78 <jimaurya6386@gmail.com>
2026-02-14 23:52:22 +0530

    Updated project files

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h className="bg-green-600 p-4">React Router</h>
+    
+
+    </>
+  )
+}
+
+export default App
